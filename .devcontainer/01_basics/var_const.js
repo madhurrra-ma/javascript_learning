a684484d5840da1ae693592f0e9904c8c25cbc28
Madhura <madhuramelgiri2006@gmail.com>
2025-03-14 01:39:22 +0530

    container config done

--- .devcontainer/01_basics/var_const.js
+++ .devcontainer/01_basics/var_const.js
@@ -1,1 +1,14 @@
+const accountId = 1234
+let accountEmail = "abc@gmail.com"
+var accountPassword = "123"
+accountCity = "pune"
+let accountState;
+
+// accountId = 12345 // this is not allowed bec it is a const
+
+accountPassword = "1212"
+accountEmail= "madhu@"
+accountCity = "solapur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
 
